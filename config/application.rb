55c93be4e9d6362846bce9c319c629874376484d
garrettrh <garrett.harris@gmail.com>
2015-01-12 21:09:23 -0800

    More heroku changes

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module MemoryBoardNew
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
   end
 end
